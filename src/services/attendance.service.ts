5c6f009441f1966f9f48f5d62602cccb0ad61dd7
Harika.padamata <harika.actimize@gmail.com>
2025-01-16 12:20:08 +0530

    date range changes

--- src/services/attendance.service.ts
+++ src/services/attendance.service.ts
@@ -18,7 +18,11 @@ export class AttendanceService {
 
   async updateAttendances(studentId: string, date: string, attendanceStatus: string) {
     try {
-      return await this.attendanceModel.updateOne({ userId: studentId, date: date }, { $set: { attendanceStatus: attendanceStatus } });
+      const startOfDay = new Date(date);
+      startOfDay.setHours(0, 0, 0, 0); // Start of the day (00:00:00.000)
+      const endOfDay = new Date(date);
+      endOfDay.setHours(23, 59, 59, 999);
+      return await this.attendanceModel.updateOne({ userId: studentId, date: { $gte: startOfDay, $lte: endOfDay } }, { $set: { attendanceStatus: attendanceStatus } });
     } catch (error) {
       throw error;
     }
