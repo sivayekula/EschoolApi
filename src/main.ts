007031932090d494c4c07d5f78cb83e8037524aa
siva yekula <siva.yekula@gmail.com>
2025-03-04 10:35:47 +0530

    added cors

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,11 @@ async function bootstrap() {
   // Increase payload size
   app.use(bodyParser.json({ limit: '10mb' })); // Adjust the size as needed
   app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
-  app.enableCors();
+  app.enableCors({
+    origin: ['https://digiakshara.com', 'http://localhost:3000', '*'], // Allow frontend domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   app.setGlobalPrefix('api/');
   await app.listen(process.env.PORT || 8001);
 }
